b9fd73edce1f739e710633328acb2eb0e1db355e
Mariia <msynoverska@webinerds.com>
2022-04-19 11:40:55 +0100

    fix: ui RewardDataWidget

--- packages/mask/src/plugins/Referral/SNSAdaptor/shared-ui/RewardDataWidget.tsx
+++ packages/mask/src/plugins/Referral/SNSAdaptor/shared-ui/RewardDataWidget.tsx
@@ -1,4 +1,4 @@
-import { Typography, Box, Grid } from '@mui/material'
+import { Typography, Grid } from '@mui/material'
 
 import { useI18N } from '../../../../utils'
 import { APR } from '../../constants'
@@ -26,41 +26,35 @@ export function RewardDataWidget({ title, icon, rewardData, tokenSymbol }: Rewar
                     </Grid>
                 </Grid>
             )}
-            <Grid item xs={4} display="flex" alignItems="center">
-                <Box>
-                    <Typography>{t('plugin_referral_estimated_apr')}</Typography>
-                    <Typography fontWeight={600} marginTop="4px">
-                        {rewardData ? APR : '-'}
-                    </Typography>
-                </Box>
+            <Grid item xs={4} display="flex" flexDirection="column">
+                <Typography>{t('plugin_referral_estimated_apr')}</Typography>
+                <Typography fontWeight={600} marginTop="4px">
+                    {rewardData ? APR : '-'}
+                </Typography>
             </Grid>
-            <Grid item xs={4} display="flex" alignItems="center">
-                <Box>
-                    <Typography>{t('plugin_referral_daily_farm_reward')}</Typography>
-                    <Typography fontWeight={600} marginTop="4px">
-                        {rewardData ? (
-                            <>
-                                {Number.parseFloat(rewardData.dailyReward.toFixed(5))} {tokenSymbol ?? '-'}
-                            </>
-                        ) : (
-                            '-'
-                        )}
-                    </Typography>
-                </Box>
+            <Grid item xs={4} display="flex" flexDirection="column">
+                <Typography>{t('plugin_referral_daily_farm_reward')}</Typography>
+                <Typography fontWeight={600} marginTop="4px">
+                    {rewardData ? (
+                        <>
+                            {Number.parseFloat(rewardData.dailyReward.toFixed(5))} {tokenSymbol ?? '-'}
+                        </>
+                    ) : (
+                        '-'
+                    )}
+                </Typography>
             </Grid>
-            <Grid item xs={4} display="flex" alignItems="center">
-                <Box>
-                    <Typography>{t('plugin_referral_total_farm_rewards')}</Typography>
-                    <Typography fontWeight={600} marginTop="4px">
-                        {rewardData ? (
-                            <>
-                                {Number.parseFloat(rewardData.totalReward.toFixed(5))} {tokenSymbol ?? '-'}
-                            </>
-                        ) : (
-                            '-'
-                        )}
-                    </Typography>
-                </Box>
+            <Grid item xs={4} display="flex" flexDirection="column">
+                <Typography>{t('plugin_referral_total_farm_rewards')}</Typography>
+                <Typography fontWeight={600} marginTop="4px">
+                    {rewardData ? (
+                        <>
+                            {Number.parseFloat(rewardData.totalReward.toFixed(5))} {tokenSymbol ?? '-'}
+                        </>
+                    ) : (
+                        '-'
+                    )}
+                </Typography>
             </Grid>
         </Grid>
     )
